4acf513f07aa4dd713845f15fa312f904f152b1b
CiroTrentacosti <c.trentacosti.esterno@almaviva.it>
2026-04-13 09:28:10 +0200

    security config

---
+++ football_API/src/main/java/com/football_API/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.football_API.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	 @Bean
+	    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+	        http
+	        	.csrf(csrf -> csrf.disable())
+	            .authorizeHttpRequests(auth -> auth
+	                .requestMatchers("/public/**").permitAll()
+	                .anyRequest().authenticated()
+	            )
+	            .oauth2ResourceServer(oauth2 -> 
+	            oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter()))
+	        );
+
+	        return http.build();
+	    }
+	 
+	 @Bean
+	 public JwtAuthenticationConverter jwtAuthenticationConverter() {
+	     JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+	     grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+	     JwtAuthenticationConverter jwtConverter = new JwtAuthenticationConverter();
+	     jwtConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+	         Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+	         Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+	         if (realmAccess != null && realmAccess.containsKey("roles")) {
+	             List<String> roles = (List<String>) realmAccess.get("roles");
+	             roles.forEach(role -> authorities.add(new SimpleGrantedAuthority("ROLE_" + role)));
+	         }
+
+	         return authorities;
+	     });
+
+	     return jwtConverter;
+	 }
+}
